a144c6dc1096b4d599adbb5b4085e7a8c0839404
Renan Bezerra Guimarães Desktop Linux <renanbezerraguima@proton.me>
2025-08-30 14:11:24 -0300

    Fixed and improved example_usage.rb

--- example_usage.rb
+++ example_usage.rb
@@ -1,13 +1,20 @@
-require 'caesar_cipher'
+require_relative 'caesar_cipher'
 
-# Sample usage
+EXAMPLE_STRING = "What a string!"
+
 puts "Cipher/Decipher ASCII:"
-
 cipher = CaesarCipher.cipher_ascii("What a string!", 5)
-puts cipher
-puts CaesarCipher.decipher_ascii(cipher, 5)
+puts "Cipher of #{EXAMPLE_STRING} = " + cipher
+puts "Decipher of #{cipher} = " + CaesarCipher.decipher_ascii(cipher, 5)
 
-puts "Brute Force Decipher ASCII:"
+puts
+
+puts "Brute Force Decipher of ASCII #{cipher}:"
 CaesarCipher.brute_force_decipher_ascii(cipher)
 
+puts
+
 puts "Cipher/Decipher Unicode:"
+cipherUnicode = CaesarCipher.cipher_unicode("What a string!", 200)
+puts "Cipher of #{EXAMPLE_STRING} = " + cipherUnicode
+puts "Decipher of #{cipherUnicode} = " + CaesarCipher.decipher_unicode(cipherUnicode, 200)
